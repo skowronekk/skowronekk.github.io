b70190b3f92fc0bcc76d700e8c200f990746093a
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-08-22 22:34:07 +0200

    update 2.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://immense-bastion-42613.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://immense-bastion-42613.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
