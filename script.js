a772d4c8da04c7ea96144a1e4c33dfbccdc1ee2e
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-10-14 21:04:12 +0200

    Application_Tasks_Final_1.2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://immense-bastion-42613.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://immense-bastion-42613.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
